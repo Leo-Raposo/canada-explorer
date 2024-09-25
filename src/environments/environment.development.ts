05056581f17b222daf6fc4c4f2c2247411c29ee5
Leo-Raposo <apoena@live.com>
2024-09-25 20:02:08 -0300

    Alteração da API Supabase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    supabaseUrl: 'https://lddcxckumtvxtojdgoth.supabase.co',
+    supabaseUrl: 'https://vuggiyeztpynqtywswgg.supabase.co',
     supabaseKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkZGN4Y2t1bXR2eHRvamRnb3RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU2NTk3OTMsImV4cCI6MjA0MTIzNTc5M30.Vs-ggBn0Q_Qqa38Vby8ivzm3pZ7H1tcB_GYK2KXmEjQ',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1Z2dpeWV6dHB5bnF0eXdzd2dnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjczMDUwNjcsImV4cCI6MjA0Mjg4MTA2N30.TMQHW1SVdYG75fn4KOlk14MYo6KTlgdMAQCk1IM6DwY',
 };
