624c14020991585e1e152bda67afe8210e41005a
Leo-Raposo <apoena@live.com>
2024-09-25 19:54:06 -0300

    Adição do production mode

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    supabaseUrl: 'https://lddcxckumtvxtojdgoth.supabase.co',
+    supabaseKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkZGN4Y2t1bXR2eHRvamRnb3RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU2NTk3OTMsImV4cCI6MjA0MTIzNTc5M30.Vs-ggBn0Q_Qqa38Vby8ivzm3pZ7H1tcB_GYK2KXmEjQ',
+};
